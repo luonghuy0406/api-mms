0be4d96bb02ca730ef95c07047aabf0c15c5b8df
Huy Nguyen <huynguyen@rtanalytics.vn>
2023-10-19 18:01:20 +0700

    update code

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3003;
 const router = require('./router');
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    next()
-  }) 
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://dashboard-mms.vercel.app/")
+    next()
+  }) 
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
