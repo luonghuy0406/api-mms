efdeab9961a437d927453fa7b141589841903649
Huy Nguyen <huynguyen@rtanalytics.vn>
2023-10-19 18:12:06 +0700

    update code

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://dashboard-mms.vercel.app/")
+    res.setHeader("Access-Control-Allow-Origin", "https://dashboard-mms.vercel.app/")
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next()
   }) 
 
